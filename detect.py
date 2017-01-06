f68c85f79944ce3cd4ac49e38f820ec893f4f816
yuanjun-jason <yuan.07a@hotmail.com>
2017-01-06 16:58:33 +0800

    A map monitor to detect PMs to bypass the limit of web scrapers

---
+++ detect.py
@@ -0,0 +1,47 @@
+import numpy as np
+from PIL import Image, ImageGrab
+import scipy.signal as ss
+from scipy.misc import toimage
+from time import sleep
+from helper import send_email
+
+#Setting
+bbox=(0,100,800,800)
+scanTime = 20
+user = 'yuan.07a@gmail.com'
+pwd = 'mesalikeitgml07!'
+thres = [-3.5, -3.5, -3., -5., -2.7]
+
+#Initialize
+kerf = []
+kerm = []
+for i in range(5):
+    tmp = np.array(Image.open( str(i+1) + '.png' )).astype(np.float32)[:,:,0:3]
+    kerf.append(tmp[::-1,::-1,::-1])
+    kerm.append(kerf[i]>0)
+
+nm = ['Snorlax', 'Chansey', 'Lapras', 'Porygon', 'Aerodactyl']
+
+#Execution
+print 'Scanning...'
+for t in range(scanTime):
+
+    im = ImageGrab.grab(bbox=bbox)
+    #im.save("foobar.png")
+    A = np.array(im).astype(np.float32)[:,:,0:3]
+    A2 = A**2
+
+    for i in range(5):
+        res = ss.fftconvolve(A2, kerm[i], 'valid') - 2*ss.fftconvolve(A, kerf[i], 'valid')
+        nres = np.squeeze(((res - np.mean(res)) / np.std(res)) < thres[i])
+        
+        if np.any(nres):
+            print nm[i] + ' Detected!'
+            im2 = toimage(nres)
+            im2.save('foobar.png')
+            break
+            
+    sleep(15)
+    print str(t)
+
+
---
+++ helper.py
@@ -0,0 +1,25 @@
+import smtplib
+
+def send_email(user, pwd, subject):
+
+    gmail_user = user
+    gmail_pwd = pwd
+    FROM = user
+    TO = user if type(user) is list else [user]
+    SUBJECT = subject
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, '')
+    try:
+        # SMTP_SSL Example
+        server_ssl = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server_ssl.ehlo() # optional, called by login()
+        server_ssl.login(gmail_user, gmail_pwd)
+        # ssl server doesn't support or need tls, so don't call server_ssl.starttls()
+        server_ssl.sendmail(FROM, TO, message)
+        #server_ssl.quit()
+        server_ssl.close()
+        print 'successfully sent the mail'
+    except:
+        print "failed to send mail"
